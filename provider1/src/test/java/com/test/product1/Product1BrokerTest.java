4649a8bdb23ff42fa3d0349c594b8ac2a5fb5589
Sridhar1974 <61754761+Sridhar1974@users.noreply.github.com>
2020-05-11 20:19:06 +0530

    End-End Pact updated

--- provider1/src/test/java/com/test/product1/Product1BrokerTest.java
+++ provider1/src/test/java/com/test/product1/Product1BrokerTest.java
@@ -2,21 +2,25 @@ package com.test.product1;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
 import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.VerificationReports;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 
 @RunWith(PactRunner.class)
-//@PactFolder("C:\\Users\\Win10\\Downloads\\New-Pact-master\\New-Pact-master\\consumer1\\target\\pacts")
+
 @Provider("EcommereceProvider")
 
-@PactBroker(host="localhost", port = "80")
-
+//@PactBroker( host = "https://sm.pact.dius.com.au/", port = "443")
+@PactBroker
+//(host = "${pactbroker.url:'https://sm.pact.dius.com.au/'}", port = "443")
+ 
 @VerificationReports(value = { "console", "markdown", "json" })
 
 public class Product1BrokerTest {
@@ -39,36 +43,21 @@ public class Product1BrokerTest {
 
 	public final Target target = new HttpTarget("http", "localhost", PORT);
 
-	// private static ConfigurableApplicationContext applicationContext;
-
-//	@BeforeClass
-//
-//	public static void start() {
-//
-	// applicationContext = SpringApplication.run(EcommerceApplication.class);
-//
-//	}
-
-//	@TargetRequestFilter
-//
-//    public void printTheRequestHeaders(HttpRequest request) {
-//
-//        Arrays.asList(request.getAllHeaders())
-//
-//                .forEach(header -> System.out.println(header.getName() + "->" + header.getValue()));
-
-	// }
-
-//	@AfterClass
-//
-//	public static void stop() {
-//
-//		SpringApplication.exit(applicationContext);
-//
-//	}
-//	
-//	
-
+	/*
+	 *  private static ConfigurableApplicationContext applicationContext;
+	 * 
+	 *  @BeforeClass // // public static void start() { // // applicationContext =
+	 * SpringApplication.run(EcommerceApplication.class); // // }
+	 * 
+	 *  @TargetRequestFilter // // public void printTheRequestHeaders(HttpRequest
+	 * request) { // // Arrays.asList(request.getAllHeaders()) // // .forEach(header
+	 * -> System.out.println(header.getName() + "->" + header.getValue()));
+	 * 
+	 *  }
+	 * 
+	 *  @AfterClass // // public static void stop() { // //
+	 * SpringApplication.exit(applicationContext); // // } // //
+	 */
 	@State("HP")
 
 	public void withSomeState() {
